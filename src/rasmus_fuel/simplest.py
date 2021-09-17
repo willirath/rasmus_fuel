4a9575273f50a092d75b2de02d6d16ac3b0f9f7b
Willi Rath <wrath@geomar.de>
2021-09-17 17:21:33 +0200

    Clarify velocity meanings

--- src/rasmus_fuel/simplest.py
+++ src/rasmus_fuel/simplest.py
@@ -3,8 +3,8 @@ import numpy.typing as npt
 
 
 def power_maintain_sog(
-    u_ship: npt.ArrayLike = None,
-    v_ship: npt.ArrayLike = None,
+    u_ship_og: npt.ArrayLike = None,
+    v_ship_og: npt.ArrayLike = None,
     u_current: npt.ArrayLike = None,
     v_current: npt.ArrayLike = None,
     drag_coeff: float = 1.0,
@@ -14,17 +14,17 @@ def power_maintain_sog(
 
     Parameters
     ----------
-    u_ship: array
+    u_ship_og: array
         Ship eastward speed over ground in meters per second.
-    v_ship: array
+    v_ship_og: array
         Ship northward speed over ground in meters per second.
-        Needs same shape as u_ship.
+        Needs same shape as u_ship_og.
     u_current: array
         Ocean currents eastward speed over ground in meters per second.
-        Needs shape that can be broadcast to shape of u_ship and v_ship.
+        Needs shape that can be broadcast to shape of u_ship_og and v_ship_og.
     v_current: array
         Ocean currents northward speed over ground in meters per second.
-        Needs shape that can be broadcast to shape of u_ship and v_ship.
+        Needs shape that can be broadcast to shape of u_ship_og and v_ship_og.
     drag_coeff: float
         Drag coefficient in arbitrary units.
 
@@ -33,19 +33,23 @@ def power_maintain_sog(
     Returns
     -------
     array:
-        Fuel consumption for each element of u_ship and v_ship.
-        Shape will be identical to u_ship and v_ship.
+        Fuel consumption for each element of u_ship_og and v_ship_og.
+        Shape will be identical to u_ship_og and v_ship_og.
 
     """
-    # ensure shapes of u_ship and v_ship agree
-    if np.array(u_ship).shape != np.array(v_ship).shape:
-        raise ValueError('Shape of u_ship and v_ship need to agree.')
+    # ensure shapes of u_ship_og and v_ship_og agree
+    if np.array(u_ship_og).shape != np.array(v_ship_og).shape:
+        raise ValueError('Shape of u_ship_og and v_ship_og need to agree.')
+
+    # calc speed through water
+    u_ship_tw = u_ship_og - u_current
+    v_ship_tw = v_ship_og - v_current
 
     # calc power to maintain speed over ground
     power_needed = (
         drag_coeff
-        * ((u_ship - u_current) ** 2 + (v_ship - v_current) ** 2) ** 0.5
-        * ((u_ship - u_current) * u_ship + (v_ship - v_current) * v_ship)
+        * (u_ship_tw ** 2 + v_ship_tw ** 2) ** 0.5
+        * (u_ship_tw * u_ship_og + v_ship_tw * v_ship_og)
     )
 
     return power_needed
--- src/rasmus_fuel/tests/test_simplest.py
+++ src/rasmus_fuel/tests/test_simplest.py
@@ -6,20 +6,20 @@ from rasmus_fuel.simplest import power_maintain_sog
 
 def test_simplest_power_maintain_sog_no_movement():
     """Check for no power if no movement is happening."""
-    assert 0 == power_maintain_sog(u_ship=0, v_ship=0, u_current=0, v_current=0)
+    assert 0 == power_maintain_sog(u_ship_og=0, v_ship_og=0, u_current=0, v_current=0)
 
 
 def test_simplest_power_maintain_sog_no_drag():
     """Check for zero power if drag is removed."""
-    u_ship = np.random.normal(size=(10,))
-    v_ship = np.random.normal(size=(10,))
+    u_ship_og = np.random.normal(size=(10,))
+    v_ship_og = np.random.normal(size=(10,))
     u_current = np.random.normal(size=(10,))
     v_current = np.random.normal(size=(10,))
     np.testing.assert_equal(
         np.zeros(shape=(10,)),
         power_maintain_sog(
-            u_ship=u_ship,
-            v_ship=v_ship,
+            u_ship_og=u_ship_og,
+            v_ship_og=v_ship_og,
             u_current=u_current,
             v_current=v_current,
             drag_coeff=0,
@@ -30,23 +30,25 @@ def test_simplest_power_maintain_sog_no_drag():
 def test_simplest_power_maintain_sog_isotropic():
     """Ensure unchanged results if swapping directions."""
     np.testing.assert_allclose(
-        power_maintain_sog(u_ship=1, u_current=5, v_ship=0, v_current=0),
-        power_maintain_sog(u_ship=0, u_current=0, v_ship=1, v_current=5),
+        power_maintain_sog(u_ship_og=1, u_current=5, v_ship_og=0, v_current=0),
+        power_maintain_sog(u_ship_og=0, u_current=0, v_ship_og=1, v_current=5),
     )
 
 
 def test_simplest_power_maintain_sog_errors():
     """Check if correct errors are raised"""
-    u_ship = np.array([1, 2, 3])
-    v_ship = np.array([1, 2])
+    u_ship_og = np.array([1, 2, 3])
+    v_ship_og = np.array([1, 2])
     with pytest.raises(ValueError) as execinfo:
-        power_maintain_sog(u_ship=u_ship, v_ship=v_ship, u_current=0, v_current=0)
-    assert str(execinfo.value) == 'Shape of u_ship and v_ship need to agree.'
+        power_maintain_sog(u_ship_og=u_ship_og, v_ship_og=v_ship_og, u_current=0, v_current=0)
+    assert str(execinfo.value) == 'Shape of u_ship_og and v_ship_og need to agree.'
 
 
 def test_simplest_power_maintain_sog_kwargs_ignored():
     """Check that arbitrary kwargs are ignored."""
     np.testing.assert_equal(
-        power_maintain_sog(u_ship=1, v_ship=1, u_current=0, v_current=0, nonexisting_kwarg=1234),
-        power_maintain_sog(u_ship=1, v_ship=1, u_current=0, v_current=0),
+        power_maintain_sog(
+            u_ship_og=1, v_ship_og=1, u_current=0, v_current=0, nonexisting_kwarg=1234
+        ),
+        power_maintain_sog(u_ship_og=1, v_ship_og=1, u_current=0, v_current=0),
     )
