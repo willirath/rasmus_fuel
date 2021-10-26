4cade14173fa17713115353d27ff2e8505721833
El Sh <eshchekinova@geomar.de>
2021-10-26 11:07:45 +0200

    Define coordinate transformation

--- src/rasmus_fuel/realistic.py
+++ src/rasmus_fuel/realistic.py
@@ -141,138 +141,8 @@ def power_maintain_sog(
 
     return power_needed
 
-
-def speed_og_from_power(
-    u_current: npt.ArrayLike = None,
-    v_current: npt.ArrayLike = None,
-    u_wind: npt.ArrayLike = None,
-    v_wind: npt.ArrayLike = None,
-    w_wave_hight: npt.ArrayLike = None,
-    engine_power: npt.ArrayLike = None,
-    vessel_waterline_width=30.0,
-    vessel_waterline_length=210.0,
-    vessel_total_propulsive_efficiency=0.7,
-    vessel_draught=11.5,
-    vessel_supersurface_area=345.0,
-    vessel_water_drag_coefficient=6000.0,
-    physics_air_mass_density=1.225,
-    vessel_wind_resistance_coefficient=0.4,
-    vessel_reference_froede_number=0.12,
-    physics_spectral_average=0.5,
-    physics_surface_water_density=1029.0,
-    physics_acceleration_gravity=9.80665,
-    **kwargs,
-) -> npt.ArrayLike:
-    """Calculate engine power from ship over ground
-
-    Parameters
-    ----------
-    u_current: array
-        Ocean currents eastward speed over ground in meters per second.
-        Needs shape that can be broadcast to shape of course_ship_og.
-    v_current: array
-        Ocean currents northward speed over ground in meters per second.
-        Needs shape that can be broadcast to shape of course_ship_og.
-    u_wind: array
-        Eastward 10 m wind in m/s
-        Needs shape that can be broadcst to shape of course_ship_og
-    v_wind: array
-        Northward 10 m wind in m/s
-        Needs shape that can be broadcst to shape of course_ship_og
-    w_wave_hight: array
-        Spectral significant wave height (Hm0), meters
-        Needs shape that can be broadcst to shape of course_ship_og
-    
-    vessel_supersurface_area: float
-        area of the above water vessel structure exposed to wind [m ** 2]. Defaults to 345
-    vessel_subsurface_area: float
-        an average area of the lateral projection of underwater vessel structure [m ** 2]. Defaults to 245
-    vessel_water_drag_coefficient: float
-        water drag coefficient for vessel [kg/m]. Defaults to 6000
-    vessel_waterline_width: float
-        width of vessel at the waterline in [m]. Defaults to 30
-    vessel_waterline_length: float
-        length of vessel at the waterline in [m]. Defaults to 210
-    vessel_total_propulsive_efficiency: float
-        total propulsive engine efficiency. Defaults to 0.7
-    vessel_draught: float
-        vessel draught in [m]. Defaults to 11.5
-    physics_air_mass_density: float
-       mass density of air [kg/m**3]. Defaults to 1.225
-    vessel_wind_resistance_coefficient: float
-       wind resistance coefficent, typically in rages [0.4-1]. Defaults to 0.4
-    physics_spectral_average: float
-       spectral and angular dependency factor, dimensionless. Defaults to 0.5
-    physics_surface_water_density: float
-       density of surface water [kg/m**3]. Defaults to 1029
-    physics_acceleration_gravity: float
-       the Earth gravity accleration [m/s**2]. Defaults to 9.80665
-
-    All other keyword arguments will be ignored.
-
-    Returns
-    -------
-    array:
-        Engine power in W (=kg*m2/s3)
-        Shape will be identical to course_ship_og.
-    """
-
-    # calc speed through water
-    u_ship_tw = u_ship_og - u_current
-    v_ship_tw = v_ship_og - v_current
-    
-    # calc speed over ground    
-    speed_og = (u_ship_og ** 2 + v_ship_og ** 2) ** 0.5
-
-    # calc engine power to maintain speed over ground using ocean current resistance term
-    speed_tw = (u_ship_tw ** 2 + v_ship_tw ** 2) ** 0.5
-
-    coeff_water_drag = (
-        0.5
-        * physics_surface_water_density
-        * vessel_water_drag_coefficient
-        * vessel_subsurface_area
-    )
-
-    power_needed = coeff_water_drag * (speed_tw ** 3)
-
-    # calc engine power to maintain speed over ground using : (1) ocean current resistance, (2) wind resistance, (3) wave resistance
-
-    # calc relative wind speed
-    speed_rel_to_wind = ((u_ship_og - u_wind) ** 2 + (v_ship_og - v_wind) ** 2) ** 0.5
-
-    coeff_wind_drag = (
-        0.5
-        * physics_air_mass_density
-        * vessel_wind_resistance_coefficient
-        * vessel_supersurface_area
-    )
-
-    coeff_wave_drag = (
-        20.0
-        * (vessel_waterline_width / vessel_waterline_length) ** (-1.2)
-        * (1 / vessel_waterline_length) ** 0.62
-        / vessel_total_propulsive_efficiency
-        / vessel_reference_froede_number
-        * physics_spectral_average
-        * physics_surface_water_density
-        * vessel_waterline_width ** 2
-        * (physics_acceleration_gravity / vessel_waterline_length ** 3) ** 0.05
-        * vessel_draught ** 0.62
-        * 0.25
-    )
-
-    power_needed = (
-        coeff_water_drag * (speed_tw ** 2) * speed_og
-        + coeff_wind_drag * speed_rel_to_wind ** 2 * speed_og
-        + coeff_wave_drag * w_wave_hight ** 2 * speed_og
-    )
-
-    return power_needed 
-
  def speed_og_from_power(
     course_ship_og: npt.ArrayLike = None,
-    speed_ship_init_og: npt.ArrayLike = None,
     u_current: npt.ArrayLike = None,
     v_current: npt.ArrayLike = None,
     u_wind: npt.ArrayLike = None,
@@ -393,20 +263,22 @@ def speed_og_from_power(
         * vessel_draught ** 0.62
         * 0.25
     )
+  
+    coeff3 = coeff_water_drag + coeff_wind_drag
+    coeff2 = (- 2 * coeff_water_drag * (u_current * np.cos(course_ship_og + np.pi/2) + v_current * np.sin(course_ship_og + np.pi/2)) 
+                  - 2 * coeff_wind_drag * (u_wind * np.cos(course_ship_og + np.pi/2) + v_wind * np.sin(course_ship_og + np.pi/2)))
+    coeff1 =  (coeff_water_drag * (u_current ** 2  + v_current ** 2) + 
+                   coeff_wind_drag * (u_wind ** 2  + v_wind ** 2) + coeff_wave_drag * w_wave_hight ** 2)  
+    coeff0 = - engine_power
+ 
+    # Make variable transformation to reduced cubic equation and solve using Cardano's formula
 
-    # TODO : design a root solver using ArrayLike structures or use formula approximation to a simple expression of speed_og from the power 
+    p = (3 * coeff3*coeff1 - coeff2 ** 2) / 3 / coeff3 ** 3
+    q = (2 * coeff2 ** 3 - 9 * coeff3 * coeff2 * coeff1 + 27 * coeff3**2 * coeff0) / 27 / coeff3 **3
 
-    def func(x):    
-        coeff3 = coeff_water_drag + coeff_wind_drag
-        coeff2 = (- 2 * coeff_water_drag * (u_current * np.cos(course_ship_og + np.pi/2) + v_current * np.sin(course_ship_og + np.pi/2)) 
-                  - 2 * coeff_wind_drag * (u_wind * np.cos(course_ship_og + np.pi/2) + v_wind * np.sin(course_ship_og + np.pi/2))  )
-        coeff1 =  (coeff_water_drag * (u_current ** 2  + v_current ** 2) + 
-                   coeff_wind_drag * (u_wind ** 2  + v_wind ** 2) + coeff_wave_drag * w_wave_hight ** 2  
-        coeff0 = - engine_power
-        
-    return (coeff3 * x ** 3 + coeff2 * x ** 2 + coeff1 * x + coeff0)
+    t = (-q/2 + np.sqrt(q ** 2  / 4 + p ** 3 / 27)) ** (-1/3)+ (-q/2 - np.sqrt(q ** 2  / 4 + p ** 3 / 27)) ** (-1/3)
 
-    speed_og = fsolve(func, [speed_ship_init_og]).max()
+    speed_og = (t - coeff2 / 3 / coeff3).max()
 
     return speed_og
 
--- src/rasmus_fuel/tests/test_realistic.py
+++ src/rasmus_fuel/tests/test_realistic.py
@@ -6,8 +6,9 @@ from rasmus_fuel.realistic import (
     power_maintain_sog,
     power_to_fuel_burning_rate,
     power_to_fuel_consump,
+    energy_efficiency_per_time_distance,
+    speed_og_from_power,
 )
-
 
 def test_realistic_power_maintain_sog_no_movement():
     """Check for no power if no movement is happening."""
@@ -113,6 +114,109 @@ def test_power_maintain_sog_kwargs_ignored():
         ),
     )
 
+def test_realistic_ speed_og_from_power_no_movement():
+    """Check for no power if no movement is happening."""
+    assert 0 ==  speed_og_from_power(
+        u_ship_og=0,
+        v_ship_og=0,
+        u_current=0,
+        v_current=0,
+        u_wind=0,
+        v_wind=0,
+        w_wave_hight=0,
+    )
+
+
+def test_realistic_ speed_og_from_power_no_drag():
+    """Check for zero power if drag is removed."""
+    u_ship_og = np.random.normal(size=(10,))
+    v_ship_og = np.random.normal(size=(10,))
+    u_current = np.random.normal(size=(10,))
+    v_current = np.random.normal(size=(10,))
+    v_wind = np.random.normal(size=(10,))
+    u_wind = np.random.normal(size=(10,))
+    w_wave_hight = np.random.normal(size=(10,))
+    np.testing.assert_equal(
+        np.zeros(shape=(10,)),
+         speed_og_from_power(
+            u_ship_og=u_ship_og,
+            v_ship_og=v_ship_og,
+            u_current=u_current,
+            v_current=v_current,
+            u_wind=u_wind,
+            v_wind=v_wind,
+            w_wave_hight=w_wave_hight,
+            vessel_water_drag_coefficient=0,
+            vessel_wind_resistance_coefficient=0,
+            vessel_draught=0,
+        ),
+    )
+
+
+def test_ speed_og_from_power_isotropic():
+    """Ensure unchanged results if swapping directions."""
+    np.testing.assert_allclose(
+        speed_og_from_power(
+            u_ship_og=1,
+            u_current=5,
+            v_ship_og=0,
+            v_current=0,
+            u_wind=1,
+            v_wind=2,
+            w_wave_hight=0,
+        ),
+        speed_og_from_power(
+            u_ship_og=0,
+            u_current=0,
+            v_ship_og=1,
+            v_current=5,
+            u_wind=1,
+            v_wind=2,
+            w_wave_hight=0,
+        ),
+    )
+
+
+def test_ speed_og_from_power_errors():
+    """Check if correct errors are raised"""
+    u_ship_og = np.array([1, 2, 3])
+    v_ship_og = np.array([1, 2])
+    with pytest.raises(ValueError) as execinfo:
+         speed_og_from_power(
+            u_ship_og=u_ship_og,
+            v_ship_og=v_ship_og,
+            u_current=0,
+            v_current=0,
+            u_wind=1,
+            v_wind=2,
+            w_wave_hight=0,
+        )
+    assert str(execinfo.value) == 'Shape of u_ship_og and v_ship_og need to agree.'
+
+
+def test_ speed_og_from_power_kwargs_ignored():
+    """Check that arbitrary kwargs are ignored."""
+    np.testing.assert_equal(
+        speed_og_from_power(
+            u_ship_og=1,
+            v_ship_og=1,
+            u_current=0,
+            v_current=0,
+            u_wind=1,
+            v_wind=2,
+            w_wave_hight=0,
+            nonexisting_kwarg=1234,
+        ),
+        speed_og_from_power(
+            u_ship_og=1,
+            v_ship_og=1,
+            u_current=0,
+            v_current=0,
+            u_wind=1,
+            v_wind=2,
+            w_wave_hight=0,
+        ),
+    )
 
 def test_power_to_fuel_consump_just_call():
     power_to_fuel_consump(engine_power=1.0, steaming_time=0.5, distance=12e3)
