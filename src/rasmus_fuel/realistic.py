728ef19472740d78519d4d3ff7852483863f7817
Willi Rath <wrath@geomar.de>
2021-11-09 16:34:36 +0100

    Fix water drag and add some typing
    Co-authored-by: eshchekinova <77109853+eshchekinova@users.noreply.github.com>

--- src/rasmus_fuel/realistic.py
+++ src/rasmus_fuel/realistic.py
@@ -9,19 +9,20 @@ def power_maintain_sog(
     v_current: npt.ArrayLike = None,
     u_wind: npt.ArrayLike = None,
     v_wind: npt.ArrayLike = None,
-    w_wave_hight: npt.ArrayLike = None,
-    vessel_waterline_width=30.0,
-    vessel_waterline_length=210.0,
-    vessel_total_propulsive_efficiency=0.7,
-    vessel_draught=11.5,
-    vessel_supersurface_area=345.0,
-    vessel_water_drag_coefficient=6000.0,
-    physics_air_mass_density=1.225,
-    vessel_wind_resistance_coefficient=0.4,
-    vessel_reference_froede_number=0.12,
-    physics_spectral_average=0.5,
-    physics_surface_water_density=1029.0,
-    physics_acceleration_gravity=9.80665,
+    w_wave_height: npt.ArrayLike = None,
+    vessel_waterline_width: float = 30.0,
+    vessel_waterline_length: float = 210.0,
+    vessel_total_propulsive_efficiency: float = 0.7,
+    vessel_maximum_engine_power: float = 14296344.0,
+    vessel_speed_calm_water: float = 9.259,
+    vessel_draught: float = 11.5,
+    vessel_supersurface_area: float = 345.0,
+    physics_air_mass_density: float = 1.225,
+    vessel_wind_resistance_coefficient: float = 0.4,
+    vessel_reference_froede_number: float = 17.6,
+    physics_spectral_average: float = 0.5,
+    physics_surface_water_density: float = 1029.0,
+    physics_acceleration_gravity: float = 9.80665,
     **kwargs,
 ) -> npt.ArrayLike:
     """Calculate quadratic drag law power needed to maintain speed over ground.
@@ -45,21 +46,23 @@ def power_maintain_sog(
     v_wind: array
         Northward 10 m wind in m/s
         Needs shape that can be broadcst to shape of u_ship_og and v_ship_og
-    w_wave_hight: array
+    w_wave_height: array
         Spectral significant wave height (Hm0), meters
         Needs shape that can be broadcst to shape of u_ship_og and v_ship_og
     vessel_supersurface_area: float
         area of the above water vessel structure exposed to wind [m ** 2]. Defaults to 345
     vessel_subsurface_area: float
         an average area of the lateral projection of underwater vessel structure [m ** 2]. Defaults to 245
-    vessel_water_drag_coefficient: float
-        water drag coefficient for vessel [kg/m]. Defaults to 6000
     vessel_waterline_width: float
         width of vessel at the waterline in [m]. Defaults to 30
     vessel_waterline_length: float
         length of vessel at the waterline in [m]. Defaults to 210
     vessel_total_propulsive_efficiency: float
         total propulsive engine efficiency. Defaults to 0.7
+    vessel_maximum_engine_power: float
+        vessel maximu engine power in [W]. Defaults to 14296344.0,
+    vessel_speed_calm_water: float
+        vessel speed maximum in calm water [m/s]. Defaults 9.259           
     vessel_draught: float
         vessel draught in [m]. Defaults to 11.5
     physics_air_mass_density: float
@@ -85,34 +88,26 @@ def power_maintain_sog(
 
     # ensure shapes of u_ship_og and v_ship_og agree
     if np.array(u_ship_og).shape != np.array(v_ship_og).shape:
-        raise ValueError('Shape of u_ship_og and v_ship_og need to agree.')
+        raise ValueError("Shape of u_ship_og and v_ship_og need to agree.")
 
-    # calc speed through water
+    # calc velocities through water
     u_ship_tw = u_ship_og - u_current
     v_ship_tw = v_ship_og - v_current
 
-    # calc engine power to maintain speed over ground using ocean current resistance term
+    # calc speeds (over ground, through water, relative to wind)
+    speed_og = (u_ship_og ** 2 + v_ship_og ** 2) ** 0.5
     speed_tw = (u_ship_tw ** 2 + v_ship_tw ** 2) ** 0.5
-
-    coeff_water_drag = (
-        0.5
-        * physics_surface_water_density
-        * vessel_water_drag_coefficient
-        * vessel_supersurface_area
-    )
-
-    power_needed = coeff_water_drag * (speed_tw ** 3)
-
-    # calc engine power to maintain speed over ground using : (1) ocean current resistance, (2) wind resistance, (3) wave resistance
-
-    # calc relative wind speed
     speed_rel_to_wind = ((u_ship_og - u_wind) ** 2 + (v_ship_og - v_wind) ** 2) ** 0.5
+
+    # drag coefficients
+    coeff_water_drag = vessel_maximum_engine_power / vessel_speed_calm_water ** 3
 
     coeff_wind_drag = (
         0.5
         * physics_air_mass_density
         * vessel_wind_resistance_coefficient
         * vessel_supersurface_area
+        / vessel_total_propulsive_efficiency
     )
 
     coeff_wave_drag = (
@@ -123,17 +118,19 @@ def power_maintain_sog(
         / vessel_reference_froede_number
         * physics_spectral_average
         * physics_surface_water_density
+        * w_wave_height ** 2
+        / 4
         * vessel_waterline_width ** 2
-        * (physics_acceleration_gravity / vessel_waterline_length ** 3) ** 0.05
+        * (physics_acceleration_gravity / vessel_waterline_length ** 3) ** 0.5
         * vessel_draught ** 0.62
         * 0.25
     )
 
-    # TODO: Check if the referecnce systems used here are correct.
+    # TODO: chec reference systems!
     power_needed = (
-        coeff_water_drag * (speed_tw ** 3)
-        + coeff_wind_drag * speed_rel_to_wind ** 2 * speed_tw
-        + coeff_wave_drag * w_wave_hight ** 2 * speed_tw
+        coeff_water_drag * (speed_tw ** 2) * speed_og
+        + coeff_wind_drag * speed_rel_to_wind ** 2 * speed_og
+        + coeff_wave_drag * speed_og ** 2
     )
 
     return power_needed
@@ -166,8 +163,8 @@ def power_to_fuel_consump(
     engine_power: npt.ArrayLike = None,
     steaming_time: npt.ArrayLike = None,
     distance: npt.ArrayLike = None,
-    vessel_specific_fuel_consumption=180.0,
-    vessel_DWT=33434.0,
+    vessel_specific_fuel_consumption: float = 180.0,
+    vessel_DWT: float = 33434.0,
 ) -> npt.ArrayLike:
     """Convert engine power to fuel consumed per vessel weight per distance and unit time.
 
@@ -191,7 +188,11 @@ def power_to_fuel_consump(
     """
 
     fuel_consump = (
-        vessel_specific_fuel_consumption * engine_power * steaming_time / vessel_DWT / distance
+        vessel_specific_fuel_consumption
+        * engine_power
+        * steaming_time
+        / vessel_DWT
+        / distance
     )
 
     return fuel_consump
